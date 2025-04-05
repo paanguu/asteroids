dae81b616a543cc33a2117ef5c8bcc641151a011
paanguu <jericho.rdd@gmail.com>
2025-04-05 16:04:39 -0500

    Draw triangle on center screen

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     print("Starting Asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -13,7 +17,9 @@ def main():
             
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
